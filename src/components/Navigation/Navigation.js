dd62fc0cd79dacc5e39d7675f887c00239fcd159
AnamulHassan <itsanamulhassan@gmail.com>
2022-09-29 06:28:40 +0600

    navigation added

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Navigation.css';
 import user from '../../img/user.jpg';
+import location from '../../img/location.png';
 
 const Navigation = () => {
   return (
@@ -11,29 +12,55 @@ const Navigation = () => {
           <p className="user-name">Mahmudul Hassan</p>
           <p className="user-location">
             <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 location-icon"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-                />
-              </svg>
+              <img className="location-user" src={location} alt="" />
             </span>
             Dhaka, Bangladesh
           </p>
         </div>
+      </div>
+      <div className="user-other-info">
+        <ul className="info-item">
+          <li>Profession</li>
+          <li>Student</li>
+        </ul>
+        <ul className="info-item">
+          <li>Age</li>
+          <li>
+            28 <span>yrs</span>
+          </li>
+        </ul>
+      </div>
+      <div className="break-btn-container">
+        <h3>Add a Break</h3>
+        <ul className="btn-break-item">
+          <li className="btn-break">
+            <span>5m</span>
+          </li>
+          <li className="btn-break">
+            <span>10m</span>
+          </li>
+          <li className="btn-break">
+            <span>10m</span>
+          </li>
+          <li className="btn-break">
+            <span>20m</span>
+          </li>
+          <li className="btn-break">
+            <span>20m</span>
+          </li>
+        </ul>
+      </div>
+      <div className="goal-details">
+        <h3>Goal Details</h3>
+        <ul>
+          <li>Working time</li>
+          <li>200 minutes</li>
+        </ul>
+        <ul>
+          <li>Break time</li>
+          <li>200 minutes</li>
+        </ul>
+        <button className="btn btn-navigation"> Activity Completed</button>
       </div>
     </div>
   );
